a83854684b3e3eaaeb92d7dfa366c8193b40db44
Dérick Pipoca <derickwfsilva@gmail.com>
2025-05-10 12:56:24 -0300

    Desafio completo

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,14 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
+        public Nokia(string numero, string modelo, string IMEI, int memoria) : base(numero, modelo, IMEI, memoria){}
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            System.Console.WriteLine("Instalando aplicativo Nokia: " + nomeApp);
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+// 1. A classe Smartphone deve ser abstrata, não permitindo instanciar e servindo apenas como modelo.
+// Smartphone smartphone = new Smartphone(); Impossível.
+
+// 2. A classe Nokia e Iphone devem ser classes filhas de Smartphone.
+System.Console.WriteLine("Iphone é filho de Smartphone: " + typeof(Iphone).IsSubclassOf(typeof(Smartphone)));
+System.Console.WriteLine("Nokia é filho de Smartphone: " + typeof(Nokia).IsSubclassOf(typeof(Smartphone)));
+
+System.Console.WriteLine();
+// 3. O método InstalarAplicativo deve ser sobrescrito na classe Nokia e iPhone, pois ambos possuem diferentes maneiras de instalar um aplicativo.
+Iphone iphone = new Iphone("+55 22 9 91000001", "12 Pro Max", "33 44 5555 6666 777", 256);
+Nokia nokia = new Nokia("+55 11 9 91000001", "Nokia 2.4", "44 55 6666 7777 888", 64);
+iphone.InstalarAplicativo("AppStore");
+nokia.InstalarAplicativo("NokiaSettings");
